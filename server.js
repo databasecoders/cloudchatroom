d59ad45da8c89e020947aa7e5a61dd7bb7abbd3f
alexcahn <alexbcahn@gmail.com>
2019-06-22 13:26:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
+var user = require("./users")
 
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 9000;
 var config = require('./config');
+
+app.get("/api/users", function (request, response) {
+    user.getAll(request, response)
+});
+
+app.get("/api/users/:id", function (request, response) {
+    user.getOne(request, response)
+});
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true }));
