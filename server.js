e18a204669fcd9ee8ebbf8d8c2141a8268aa0b7f
alexcahn <alexbcahn@gmail.com>
2019-06-22 12:43:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 9000;
+var PORT = process.env.PORT || 9000;
 var config = require('./config');
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(port);
-
 app.route('/').get(function (request, response) {
     response.json(config);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
