706ae94527fe5b2f2b116c24f832a15123ec3f2a
alexcahn <alexbcahn@gmail.com>
2019-06-23 08:45:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 require('dotenv').config();
-var user = require("./users")
+var user = require("./models/users")
 
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 9000;
-var config = require('./config');
+var config = require('./config/config');
 
-app.get("/api/users", function (request, response) {
-    user.getAll(request, response)
-});
-
-app.get("/api/users/:id", function (request, response) {
-    user.getOne(request, response)
-});
+require("./routes/api-routes")(app);
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.route('/').get(function (request, response) {
-    response.json(config);
-});
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
