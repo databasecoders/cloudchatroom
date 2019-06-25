e66a3bdb0e9edb186eb7737f80eefb90704d1e3c
arg <anna.gaukel@gmail.com>
2019-06-25 18:33:41 -0500

    prof page db connect

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
+var path = require('path')
 
 //Dependencies
 var express = require('express');
@@ -7,7 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 9000;
 // var config = require('./config/config');
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -20,6 +21,13 @@ app.use(express.json());
 
 require("./routes/api-routes")(app);
 
+app.get("/", function (rend, res) {
+    res.sendFile(path.join(__dirname, "./main.html"))
+});
+
+app.get("/profile", function (rend, res) {
+    res.sendFile(path.join(__dirname, "./profile.html"))
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
