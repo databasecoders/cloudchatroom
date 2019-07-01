6a3f99a01dc5caa6e9efaf3b60f4fe06ad854cba
alexcahn <alexbcahn@gmail.com>
2019-07-01 07:45:19 -0500

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const user = require("./../models/users")
-const loggedin = require("./../models/user-login")
+const log = require("./../models/user-login")
 
 module.exports = function (app) {
 
@@ -16,11 +16,11 @@ module.exports = function (app) {
     });
 
     app.post("/api/users/login", function (request, response) {
-        loggedin.login(request, response)
+        log.login(request, response)
     })
 
     app.delete("/api/users/logout", function (request, response) {
-        loggedin.logout(request, response)
+        log.logout(request, response)
     })
 
     app.delete("/api/users/", function (request, response) {
