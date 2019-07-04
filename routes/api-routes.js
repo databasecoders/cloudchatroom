c1e9e4b2ffcbb29b15cf24c5c565075f3b97a399
alexcahn <alexbcahn@gmail.com>
2019-07-04 06:24:30 -0500

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -46,19 +46,24 @@ module.exports = function (app) {
     //     user.updateOne(request, response)
     // })
 
-    app.post("/api/user", function (request, response) {
+    app.post('/api/user', function (request, response) {
         user.create(request, response);
     });
-    app.post("/api/user/login", function (request, response) {
+    app.post('/api/user/login', function (request, response) {
         user.login(request, response);
     });
-    app.delete("/api/user/login", function (request, response) {
+
+    app.get('/api/user/logout', function (request, response) {
         user.logout(request, response);
     });
-    app.get("/api/user", authorizer.authenticate, function (request, response) {
+    app.get('/api/user', authorizer.authenticate, function (request, response) {
         user.getMyself(request, response);
     });
-    app.get("/api/user/:id", authorizer.authenticate, function (request, response) {
+
+    app.get('/api/user/:id', authorizer.authenticate, function (
+        request,
+        response
+    ) {
         user.getUserByID(request, response);
     });
 };
