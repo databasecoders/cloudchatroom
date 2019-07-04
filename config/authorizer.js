b71d9c1f41ad8493f07a3d1d37ac0f05d0c245db
alexcahn <alexbcahn@gmail.com>
2019-07-04 06:24:47 -0500

    Update authorizer.js

--- config/authorizer.js
+++ config/authorizer.js
@@ -2,19 +2,24 @@ let users = require('../models/user-login');
 
 let authorizer = {
     authenticate: function (request, response, next) {
-        users.getMyself(request.headers['x-session-token'], function (error, result) {
+        users.getMyself(request.cookies['x_session_token'], function (
+            error,
+            result
+        ) {
             if (error) {
                 console.log(error);
                 response.status(500).json({
-                    'error': 'oops we did something bad'
+                    error: 'oops we did something bad'
                 });
             } else if (!result.length) {
-                response.redirect("/api/users/login");
+                response.status(401).json({
+                    error: 'user must be logged in'
+                });
             } else {
                 next();
             }
         });
-    },
+    }
 };
 
 module.exports = authorizer;
