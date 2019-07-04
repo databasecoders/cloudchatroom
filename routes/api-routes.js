e7bd12ea66c2fa9e1f056aaeaa84b555d50cd622
alexcahn <alexbcahn@gmail.com>
2019-07-04 12:30:09 -0500

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,49 @@
 const user = require("./../models/user-login")
 const chat = require("../FirebaseStuff/firebase-chat");
 const authorizer = require("../config/authorizer")
+let users = require('../models/users.js');
 
 module.exports = function (app) {
+    app.post('/api/user', function (request, response) {
+        user.create(request, response);
+    });
+    app.post('/api/user/login', function (request, response) {
+        user.login(request, response);
+    });
 
+    app.get('/api/user/logout', function (request, response) {
+        user.logout(request, response);
+    });
+    app.get('/api/user', function (request, response, next) {
+
+        users.getMyself(request.cookies['x_session_token'], function (error, result) {
+            if (error) {
+                console.log(error);
+                response.status(500).json({
+                    error: 'oops we did something bad'
+                });
+            } else if (!result.length) {
+                response.status(401).json({
+                    error: 'user must be logged in'
+                });
+            } else {
+                next();
+            }
+        })
+    }, function (request, response) {
+        // user.getMyself(request, response);
+        users.getMyself(request.cookies['x_session_token'], function (error, result) {
+            response.json(result[0]);
+        });
+
+    });
+
+    app.get('/api/user/:id', authorizer.authenticate, function (
+        request,
+        response
+    ) {
+        user.getUserByID(request, response);
+    });
     app.get("/api/chat", function (request, response) {
         console.log("This api route was hit");
         console.log("Type: Get")
@@ -46,27 +86,9 @@ module.exports = function (app) {
     //     user.updateOne(request, response)
     // })
 
-    app.post('/api/user', function (request, response) {
-        user.create(request, response);
-    });
-    app.post('/api/user/login', function (request, response) {
-        user.login(request, response);
-    });
 
-    app.get('/api/user/logout', function (request, response) {
-        user.logout(request, response);
-    });
-    app.get('/api/user', authorizer.authenticate, function (request, response) {
-        user.getMyself(request, response);
-    });
 
-    app.get('/api/user/:id', authorizer.authenticate, function (
-        request,
-        response
-    ) {
-        user.getUserByID(request, response);
-    });
-};
+}
 
 
 //multer (research)
