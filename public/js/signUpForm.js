64723bc58339ec494cb16d249787c298a4032ff7
alexcahn <alexbcahn@gmail.com>
2019-07-05 08:39:03 -0500

    Update signUpForm.js

--- public/js/signUpForm.js
+++ public/js/signUpForm.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
     $(".signUpButton").on("click", function () {
         event.preventDefault()
-        $("#usernameInvalid").hide()
+        $("#nameInvalid").hide()
         $("#emailInvalid").hide()
         $("#passwordEnter").hide()
         $("#passwordInvalid").hide()
         $("#checkboxInvalid").hide()
+        $("#bioInvalid").hide()
 
+        let name = $("#name").val()
         let password = $("#password").val();
         let confirmPassword = $("#confirmPassword").val();
         let email = $("#email").val();
+        let bio = $("#userBio").val()
+        console.log(bio)
 
-        if (!email.includes('@') || !email.includes('.')) {
+        if (name == "") {
+            $("#nameInvalid").show()
+        } else if (!email.includes('@') || !email.includes('.')) {
             $("#emailInvalid").show()
         } else if (password == "" || confirmPassword == "") {
             $("#passwordEnter").show()
         } else if (password !== confirmPassword) {
             $("#passwordInvalid").show()
+        } else if (bio == "") {
+            $("#bioInvalid").show()
         } else if (!$("#checkbox").is(":checked")) {
             $("#checkboxInvalid").show()
         } else {
@@ -25,6 +33,7 @@ $(document).ready(function () {
                 password: $("#password").val().trim(),
                 password_confirm: $("#confirmPassword").val().trim(),
                 email: $("#email").val().trim(),
+                bio: $("#userBio").val().trim(),
                 user_image: localStorage.getItem("user_image")
             }
             console.log('*****', newUserInfo);
