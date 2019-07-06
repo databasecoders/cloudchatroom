e1d1101ca76ebbde7c67e224086b2db83fffb87d
alexcahn <alexbcahn@gmail.com>
2019-07-05 23:27:55 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -40,19 +40,17 @@ connection.connect(function (err) {
 var admin = require("firebase-admin");
 
 var firebaseCred = {
-type: process.env.type,
-project_id: process.env.project_id,
-private_key_id: process.env.private_key_id,
-private_key: process.env.private_key.replace(/\\n/g, '\n'),
-client_email: process.env.client_email,
-client_id: process.env.client_id,
-auth_uri: process.env.auth_uri,
-token_uri: process.env.token_uri,
-auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-client_x509_cert_url: process.env.client_x509_cert_url
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
 };
-
-//console.log(firebaseCred);
 
 admin.initializeApp({
     credential: admin.credential.cert(firebaseCred),
@@ -61,8 +59,6 @@ admin.initializeApp({
 
 
 var database = admin.database().ref();
-//var userRef = database.ref("ChatLog/user_id/textLog"); //TO DO: Tweak to point to appropriate user ID text logs with ${}
-//var textRef = database.ref()
 
 //Firebase End
 
