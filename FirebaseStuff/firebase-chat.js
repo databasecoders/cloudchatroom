6a25aab791e327e630a1f9b1ba281045da1236a4
alexcahn <alexbcahn@gmail.com>
2019-07-05 23:28:02 -0500

    Update firebase-chat.js

--- FirebaseStuff/firebase-chat.js
+++ FirebaseStuff/firebase-chat.js
@@ -1,29 +1,17 @@
 let configObject = require("../config/config");
-
 let Chatref = configObject.database;
-
-//database.ref("ChatLog/user_id/textLog");
 let chat = {
-    gettingChat : function(request, response){
-        Chatref.once("value", function(snapshot, prevChildKey){  //TO-DO: Test grabbing different user chat-logs with different IDs
+    gettingChat: function (request, response) {
+        Chatref.once("value", function (snapshot, prevChildKey) {
             var Chatlog = snapshot.val();
             response.json(Chatlog);
-            
-            // console.log("Log: " + Chatlog.log);
-            // console.log("Time: " + Chatlog.time);
-            // console.log("Previous Post ID: " + prevChildKey) //TO-DO: Find out what this means
         });
     },
-    postingText : function(request, response){
+    postingText: function (request, response) {
         Chatref.child("textlog").push({
-            //time: request.body.time
             log: request.body.userInput
         })
     }
 }
-
-
-
-//gettingChat();
 
 module.exports = chat;
